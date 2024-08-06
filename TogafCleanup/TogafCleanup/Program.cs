0b504067a0084d0d20a80e3d82fd55e526f67b0c
Pieter van Ginkel <pvginkel@gmail.com>
2024-08-06 20:58:31 +0200

    Initial commit.

---
+++ TogafCleanup/TogafCleanup/Program.cs
@@ -0,0 +1,104 @@
+﻿using HtmlAgilityPack;
+
+var input = args[0];
+var output = args[1];
+
+foreach (var file in Directory.GetFiles(input, "*", SearchOption.AllDirectories))
+{
+    Console.WriteLine(file.Substring(input.Length));
+
+    var target = output + file.Substring(input.Length);
+
+    Directory.CreateDirectory(Path.GetDirectoryName(target));
+
+    if (Path.GetExtension(file) == ".html")
+    {
+        var doc = new HtmlDocument();
+        doc.Load(file);
+
+        // Rewrite the title.
+        if (Path.GetFileName(file) == "index.html")
+        {
+            var title = doc.DocumentNode.DescendantsAndSelf("title").Single(p => p.Name == "title");
+            var titleText = $"TOGAF® Standard 10 - {HtmlEntity.DeEntitize(title.InnerText)}";
+
+            var toc = doc.GetElementbyId("toc");
+            if (toc != null)
+            {
+                // Rewrite the title.
+                var indexRef = toc.DescendantsAndSelf("a").SingleOrDefault(p => p.GetAttributeValue("href", null) == "index.html" && !string.IsNullOrWhiteSpace(p.InnerText));
+                if (indexRef != null)
+                    titleText = $"TOGAF® Standard 10 - {HtmlEntity.DeEntitize(indexRef.InnerText)}";
+            }
+
+            ((HtmlTextNode)title.FirstChild).Text = titleText;
+
+            // Write an ePub metadata file that the pandoc generation script can pick up.
+
+            File.WriteAllText(
+                Path.Combine(Path.GetDirectoryName(target), "metadata.xml"),
+                """
+                <dc:language>en-us</dc:language> 
+                <dc:creator>The Open Group</dc:creator> 
+                <dc:rights>Copyright © 1999-2022 The Open Group, All Rights Reserved.</dc:rights>
+                """
+            );
+
+            File.WriteAllText(Path.Combine(Path.GetDirectoryName(target), "title.txt"), titleText);
+        }
+
+        // Fix the content type.
+        foreach (var tag in doc.DocumentNode.DescendantsAndSelf("meta"))
+        {
+            if (tag.GetAttributeValue("http-equiv", null) == "Content-Type")
+                tag.SetAttributeValue("content", tag.GetAttributeValue("content", "").Replace("iso-8859-1", "utf-8"));
+        }
+
+        // Remove the header and toc.
+        doc.GetElementbyId("toc")?.Remove();
+        doc.GetElementbyId("header")?.Remove();
+
+        // Remove chapter toc.
+        var content = doc.GetElementbyId("content");
+        if (content != null)
+        {
+            var inToc = false;
+            foreach (var el in content.ChildNodes.ToList())
+            {
+                if (el is HtmlCommentNode cn && cn.Comment.Contains("chapter toc start"))
+                    inToc = true;
+                if (inToc)
+                    el.Remove();
+                if (el is HtmlCommentNode cn1 && cn1.Comment.Contains("chapter toc end"))
+                    inToc = false;
+            }
+        }
+
+        // Remove footer.
+        var returnToTop = doc.DocumentNode.DescendantsAndSelf("div").SingleOrDefault(p => p.HasClass("returntotop"));
+        if (returnToTop != null)
+        {
+            var hadEl = false;
+            foreach (var el in returnToTop.ParentNode.ChildNodes.ToList())
+            {
+                if (el == returnToTop)
+                    hadEl = true;
+                if (hadEl)
+                    el.Remove();
+            }
+        }
+
+        // Remove the content div.
+        if (content != null)
+        {
+            content.ParentNode.AppendChildren(content.ChildNodes);
+            content.Remove();
+        }
+
+        doc.Save(target);
+    }
+    else
+    {
+        File.Copy(file, target, true);
+    }
+}
---
+++ run.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+set -e
+
+cd "$(dirname "$0")"
+
+ROOT=$(pwd)
+
+download_togaf() {
+    if [ -d tmp/togaf ]; then
+        return
+    fi
+
+    mkdir -p $ROOT/tmp/togaf
+    cd $ROOT/tmp/togaf
+
+    wget \
+        --mirror \
+        --convert-links \
+        --adjust-extension \
+        --page-requisites \
+        --no-parent \
+        --restrict-file-names=unix \
+        --no-host-directories --cut-dirs=1 \
+        https://pubs.opengroup.org/togaf-standard/
+}
+
+build_togafcleanup() {
+    cd $ROOT/TogafCleanup
+
+    docker build -t togafcleanup .
+}
+
+run_togafcleanup() {
+    cd $ROOT
+
+    docker run -v $ROOT/tmp:/work togafcleanup /work/togaf /work/togaf-rewritten
+}
+
+run_pandoc() {
+    cd $ROOT
+
+    mkdir -p $ROOT/out
+
+    for f in $(find $ROOT/tmp/togaf-rewritten -name metadata.xml); do
+        cd "$(dirname "$f")"
+        pandoc \
+            --standalone \
+            $(find . -type d -printf "--resource-path=%f ") \
+            --output "$ROOT/out/$(cat title.txt).epub" \
+            --toc \
+            --epub-cover-image="$ROOT/tmp/togaf/adm/Figures/adm.png" \
+            --epub-metadata="$f" \
+            $(find . -name "*.html" | sort)
+    done
+}
+
+download_togaf
+build_togafcleanup
+run_togafcleanup
+run_pandoc
